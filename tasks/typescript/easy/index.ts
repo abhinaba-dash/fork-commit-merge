fafabeb4a65d511e1a46dd7bde3eb28fb6fc7a89
Abhinaba Dash <abhinabadash19@gmail.com>
2023-10-01 21:17:00 +0530

    Updated index.ts

--- tasks/typescript/easy/index.ts
+++ tasks/typescript/easy/index.ts
@@ -1,7 +1,8 @@
 // TypeScript - Easy
+// TypeScript program which has a function named greet which can take a string argument name and return a greeting message.
 
 function greet(name: string): string {
-    // TODO: Add return statement
+    return `Hello, ${name}!`;
 }
 
 let user: string = 'John Doe';
